575b08d6d8a63ee1e6b7725762554dde676a5e94
Nivesh Wanninayaka_Niva98 <niveshncwanninayaka@gmail.com>
2023-05-04 12:19:07 +0530

    QR scanner step 2

--- components/screens/QRScreen.tsx
+++ components/screens/QRScreen.tsx
@@ -5,23 +5,39 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
+import {Padding} from '@mui/icons-material';
 
 export const QRScreen = () => {
+  const [data, SetData] = React.useState('scan something');
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <SafeAreaView>
-        <View style={styles.menu}>
-          <View>
-            <Icon name="menu" size={24} color={'#fff'} />
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <View style={styles.menu}>
+            <View>
+              <Icon name="menu" size={24} color={'#fff'} />
+            </View>
+            <View style={styles.homeScreen}>
+              <Text style={styles.homeScreenText}>QR Scan</Text>
+            </View>
           </View>
-          <View style={styles.homeScreen}>
-            <Text style={styles.homeScreenText}>QR Scan</Text>
+          <View style={{flex: 1, height: '60%'}}>
+            <QRCodeScanner
+              onRead={({data}) => SetData(data)}
+              flashMode={RNCamera.Constants.FlashMode.off}
+              reactivate={true}
+              reactivateTimeout={500}
+              topContent={
+                <View>
+                  <Text>{data}</Text>
+                </View>
+              }
+              showMarker={true}
+              containerStyle={{flex: 1}}
+              cameraContainerStyle={{flex: 1, padding: 50, height: '60%'}}
+            />
           </View>
         </View>
-        <QRCodeScanner
-          onRead={({data}) => Alert.alert(data)}
-          flashMode={RNCamera.Constants.FlashMode.torch}
-        />
       </SafeAreaView>
     </ScrollView>
   );
@@ -39,127 +55,12 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
   },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',
-    padding: 5,
-  },
-  image: {
-    width: 75,
-    height: 75,
-    marginBottom: 20,
-  },
-  inputViewEmail: {
-    backgroundColor: '#d2691e',
-    flexDirection: 'row',
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-    paddingLeft: 8,
-    marginBottom: 4,
-    width: '95%',
-    height: 35,
-    borderRadius: 30,
-    alignSelf: 'center',
-    alignItems: 'center',
-  },
-  inputViewPassword: {
-    backgroundColor: '#d2691e',
-    flexDirection: 'row',
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-    paddingLeft: 8,
-    marginBottom: 4,
-    width: '95%',
-    height: 35,
-    borderRadius: 30,
-    alignSelf: 'center',
-    alignItems: 'center',
-  },
-  TextInputEmail: {
-    height: 50,
-    flex: 1,
-  },
-  TextInputPassword: {
-    height: 50,
-    flex: 1,
-  },
-  forgot_button: {
-    height: 30,
-  },
-  loginBtn: {
-    width: '85%',
-    borderRadius: 25,
-    height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#800000',
-    marginBottom: 10,
-  },
-  loginText: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  buttonGroup: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  register_button: {
-    height: 30,
-    marginBottom: 40,
-  },
-  guestTouchable: {
-    backgroundColor: '#800000',
-    padding: 5,
-    width: '95%',
-    height: 40,
-    borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  guestText: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#fff',
-    fontFamily: 'Roboto-Italic',
-  },
-  loginLogo: {
-    flexDirection: 'row',
-    borderColor: '#ddd',
-    borderWidth: 2,
-    borderRadius: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    width: '95%',
-    justifyContent: 'center',
-  },
-  facebookLogo: {
-    width: 30,
-    height: 30,
-    marginRight: 10,
-  },
-  googleLogo: {
-    width: 30,
-    height: 30,
-  },
-  registerText: {
-    fontFamily: 'Roboto-Medium',
-    fontSize: 28,
-    fontWeight: '500',
-    color: '#333',
-    marginBottom: 20,
-  },
   menu: {
     flex: 1,
     backgroundColor: '#dc143c',
-    padding: 5,
     flexDirection: 'row',
+    width: '100%',
+    marginBottom: 30,
+    padding: 5,
   },
 });
