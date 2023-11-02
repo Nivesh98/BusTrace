f7e0222f4ad14eb82fcea7796bfa4491bc0b0104
Nivesh Wanninayaka_Niva98 <niveshncwanninayaka@gmail.com>
2023-11-02 21:42:00 +0530

    TimeTable

--- components/CustomComponent/DropDown.tsx
+++ components/CustomComponent/DropDown.tsx
@@ -1,153 +1,3 @@
-// import React, {useRef, useState} from 'react';
-// import {
-//   FlatList,
-//   Image,
-//   Text,
-//   TextInput,
-//   TouchableOpacity,
-//   View,
-// } from 'react-native';
-
-// interface Item {
-//   value: string;
-//   key: string;
-// }
-
-// interface DropDownProps {
-//   onSelectionChange?: ((selectedItem: string) => void) | undefined;
-//   data: Item[]; // Pass the data as a prop
-// }
-
-// const DropDown: React.FC<DropDownProps> = ({onSelectionChange, data}) => {
-//   const [search, setSearch] = useState<string>('');
-//   const [clicked, setClicked] = useState<boolean>(false);
-//   const [filteredData, setFilteredData] = useState<Item[]>(data);
-//   const [selectedCountry, setSelectedCountry] = useState<string>('');
-//   const searchRef = useRef<TextInput | null>(null);
-
-//   const onSearch = (search: string) => {
-//     let tempData;
-//     if (search !== '') {
-//       tempData = data.filter(item => {
-//         console.log('item', item);
-//         return item.value.toLowerCase().indexOf(search.toLowerCase()) > -1;
-//       });
-//       setFilteredData(tempData);
-//     } else {
-//       setFilteredData(data);
-//     }
-//     console.log('data', data);
-//     console.log('tempData', tempData);
-//   };
-
-//   const handleSelection = (item: Item) => {
-//     setSelectedCountry(item.value);
-//     setClicked(false);
-//     if (onSelectionChange) {
-//       onSelectionChange(item.value);
-//     }
-//     onSearch('');
-//     setSearch('');
-//   };
-
-//   return (
-//     <View>
-//       <TouchableOpacity
-//         style={{
-//           width: '90%',
-//           height: 50,
-//           borderRadius: 10,
-//           borderWidth: 0.5,
-//           alignSelf: 'center',
-//           flexDirection: 'row',
-//           justifyContent: 'space-between',
-//           alignItems: 'center',
-//           paddingLeft: 15,
-//           paddingRight: 15,
-//         }}
-//         onPress={() => {
-//           setClicked(!clicked);
-//         }}>
-//         <Text style={{fontWeight: '600'}}>
-//           {selectedCountry == '' ? 'Select Country' : selectedCountry}
-//         </Text>
-//         {clicked ? (
-//           <Image
-//             source={require('./../assets/images/facebook.png')}
-//             style={{width: 20, height: 20}}
-//           />
-//         ) : (
-//           <Image
-//             source={require('./../assets/images/google.png')}
-//             style={{width: 20, height: 20}}
-//           />
-//         )}
-//       </TouchableOpacity>
-//       {clicked ? (
-//         <View
-//           style={{
-//             elevation: 5,
-//             marginTop: 5,
-//             height: 200,
-//             alignSelf: 'center',
-//             width: '90%',
-//             backgroundColor: '#fff',
-//             borderRadius: 10,
-//           }}>
-//           <TextInput
-//             placeholder="Search.."
-//             value={search}
-//             ref={searchRef}
-//             onChangeText={txt => {
-//               onSearch(txt);
-//               setSearch(txt);
-//             }}
-//             style={{
-//               width: '90%',
-//               height: 40,
-//               alignSelf: 'center',
-//               borderWidth: 0.2,
-//               borderColor: '#8e8e8e',
-//               borderRadius: 7,
-//               marginTop: 5,
-//               paddingLeft: 20,
-//             }}
-//           />
-
-//           <FlatList
-//             data={filteredData}
-//             renderItem={({item, index}) => {
-//               return (
-//                 <TouchableOpacity
-//                   style={{
-//                     width: '85%',
-//                     alignSelf: 'center',
-//                     height: 50,
-//                     justifyContent: 'center',
-//                     borderBottomWidth: 0.5,
-//                     borderColor: '#8e8e8e',
-//                   }}
-//                   onPress={() => {
-//                     setSelectedCountry(item.value);
-//                     setClicked(!clicked);
-//                     onSearch('');
-//                     setSearch('');
-//                   }}>
-//                   <Text style={{fontWeight: '600'}}>{item.value}</Text>
-//                 </TouchableOpacity>
-//               );
-//             }}
-//           />
-//         </View>
-//       ) : null}
-//     </View>
-//   );
-// };
-
-// export default DropDown;
-
-// DropDown.tsx
-
 import React, {useEffect, useRef, useState} from 'react';
 import {
   FlatList,
@@ -232,12 +82,12 @@ const DropDown: React.FC<DropDownProps> = ({
         </Text>
         {clicked ? (
           <Image
-            source={require('./../assets/images/facebook.png')}
+            source={require('./../assets/images/arrow_up.png')}
             style={{width: 20, height: 20}}
           />
         ) : (
           <Image
-            source={require('./../assets/images/google.png')}
+            source={require('./../assets/images/arrow_down.png')}
             style={{width: 20, height: 20}}
           />
         )}
@@ -246,12 +96,13 @@ const DropDown: React.FC<DropDownProps> = ({
         <View
           style={{
             elevation: 5,
-            marginTop: 5,
+            marginTop: 50,
             height: 200,
             alignSelf: 'center',
             width: '90%',
             backgroundColor: '#fff',
             borderRadius: 10,
+            position: 'absolute',
           }}>
           <TextInput
             placeholder="Search.."
--- components/screens/Passenger/TimeTable.tsx
+++ components/screens/Passenger/TimeTable.tsx
@@ -1,73 +1,6 @@
-// import React, {useState} from 'react';
-// import {Button, StyleSheet, View} from 'react-native';
-// import DropDown from '../../CustomComponent/DropDown';
-
-// const countries = [
-//   {value: 'Benin', key: '229'},
-//   {value: 'Bermuda', key: '1-441'},
-//   {value: 'Bhutan', key: '975'},
-//   {value: 'Bolivia', key: '591'},
-//   {value: 'Bosnia and Herzegovina', key: '387'},
-//   {value: 'Botswana', key: '267'},
-// ];
-
-// const TimeTable: React.FC = () => {
-//   const [selectedCountry1, setSelectedCountry1] = useState<string>('');
-//   const [selectedCountry2, setSelectedCountry2] = useState<string>('');
-
-//   const handleSelection1 = (selectedItem: any) => {
-//     console.log('selectedItem', selectedItem);
-//     setSelectedCountry1(selectedItem.value);
-//   };
-
-//   const handleSelection2 = (selectedItem: any) => {
-//     console.log('selectedItem', selectedItem);
-//     setSelectedCountry2(selectedItem.value);
-//   };
-
-//   const swapSelections = (country1: string, country2: string) => {
-//     // Implement your logic to swap selections here
-//     console.log(`Swapping selections: ${country1} and ${country2}`);
-//     // setSelectedCountry1(country2);
-//     // setSelectedCountry2(country1);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.dropdownContainer}>
-//         <DropDown data={countries} />
-//       </View>
-//       <View style={styles.dropdownContainer}>
-//         <DropDown data={countries} />
-//       </View>
-
-//       <Button
-//         title="Swap"
-//         onPress={() => swapSelections(selectedCountry1, selectedCountry2)}
-//       />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   dropdownContainer: {
-//     paddingHorizontal: 20,
-//     paddingVertical: 5,
-//     width: '100%',
-//   },
-// });
-
-// export default TimeTable;
-
-// TimeTable.tsx
-
+import {Text} from '@react-native-material/core';
 import React, {useState} from 'react';
-import {Button, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 import DropDown from '../../CustomComponent/DropDown';
 
 const countries = [
@@ -108,18 +41,25 @@ const TimeTable: React.FC = () => {
           selectedValue={selectedCountry1} // Pass the selected value
         />
       </View>
-      <View style={styles.dropdownContainer}>
+      <TouchableOpacity
+        style={styles.swapBtn}
+        onPress={() => swapSelections(selectedCountry1, selectedCountry2)}>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.swaptext}>Swap</Text>
+          <Image
+            source={require('./../../assets/images/swap_icon.png')}
+            style={{width: 20, height: 20}}
+          />
+        </View>
+      </TouchableOpacity>
+
+      <View style={styles.dropdownContainer2}>
         <DropDown
           onSelectionChange={handleSelection2}
           data={countries}
           selectedValue={selectedCountry2} // Pass the selected value
         />
       </View>
-
-      <Button
-        title="Swap"
-        onPress={() => swapSelections(selectedCountry1, selectedCountry2)}
-      />
     </View>
   );
 };
@@ -127,13 +67,30 @@ const TimeTable: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
   },
   dropdownContainer: {
-    paddingHorizontal: 20,
-    paddingVertical: 5,
     width: '100%',
+    zIndex: 2,
+    marginTop: 10,
+  },
+  dropdownContainer2: {
+    width: '100%',
+    zIndex: 1,
+  },
+  swapBtn: {
+    alignSelf: 'flex-end',
+    marginRight: 19,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    marginBottom: 4,
+    marginTop: 4,
+    padding: 1,
+  },
+  swaptext: {
+    fontSize: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
