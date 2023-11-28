e5954b7e078b6a0d1c128ef543f5fa959e0293a8
Nivesh Wanninayaka_Niva98 <niveshncwanninayaka@gmail.com>
2023-11-29 02:22:31 +0530

    add image Picker

--- components/screens/Passenger/PassengerSettingDrawer.tsx
+++ components/screens/Passenger/PassengerSettingDrawer.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
+import React, {useState} from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import ImagePicker from 'react-native-image-crop-picker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 const PassengerSettingDrawer = () => {
+  const handleImageSelection = async () => {
+    ImagePicker.openPicker({
+      width: 400,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+    });
+  };
+
+  const [selectedImage, setSelectedImage] = useState(
+    'https://www.kasandbox.org/programming-images/avatars/old-spice-man.png',
+  );
+
   return (
-    <View>
-      <Text>PassengerSettingDrawer</Text>
+    <View style={{alignItems: 'center', marginVertical: 22}}>
+      <TouchableOpacity onPress={handleImageSelection}>
+        <Image
+          source={{uri: selectedImage}}
+          style={{
+            height: 170,
+            width: 170,
+            borderRadius: 85,
+            borderWidth: 5,
+            borderColor: '#fc0303',
+          }}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 10,
+            zIndex: 9999,
+          }}>
+          <Icon
+            name="add-a-photo"
+            size={43}
+            color="#cc2f2f"
+            style={{padding: 2}}
+          />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
