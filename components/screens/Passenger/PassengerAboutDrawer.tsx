8c9196e7adbf63abcead438a18316e75d9636298
Nivesh Wanninayaka_Niva98 <niveshncwanninayaka@gmail.com>
2023-12-02 22:08:04 +0530

    Create About Support component

--- components/screens/Passenger/PassengerAboutDrawer.tsx
+++ components/screens/Passenger/PassengerAboutDrawer.tsx
@@ -2,13 +2,50 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 const PassengerAboutDrawer = () => {
+  const appInfo = {
+    name: 'BusTrace',
+    version: '1.0.0',
+    description:
+      "BusTrace is a mobile based bus tracking and booking system. we provide you with the facilities of tracking the busses' location, book seats and do the payments for the tickets in your own convenience which is available 24/7 and economical",
+    // Add more app related information here
+  };
+
   return (
-    <View>
-      <Text>PassengerAboutDrawer</Text>
+    <View style={styles.content}>
+      <Text style={styles.title}>{appInfo.name}</Text>
+      <Text style={styles.info}>Version: {appInfo.version}</Text>
+      <Text style={styles.info1}>{appInfo.description}</Text>
+      {/* Render more app information here */}
     </View>
   );
 };
 
 export default PassengerAboutDrawer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  info: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  info1: {
+    fontSize: 16,
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  // Add more styles if needed
+});
--- components/screens/Passenger/PassengerSupportDrawer.tsx
+++ components/screens/Passenger/PassengerSupportDrawer.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Linking, Platform, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const PassengerSupportDrawer = () => {
+  const phoneNumber = '0767089918';
+
+  const makeCall = () => {
+    let phoneNumber = '';
+
+    // Check if the platform is iOS or Android
+    if (Platform.OS === 'android') {
+      phoneNumber = 'tel:${0767089918}';
+    } else {
+      phoneNumber = 'telprompt:${0767089918}';
+    }
+
+    // Use the Linking API to open the phone app
+    Linking.openURL(phoneNumber);
+  };
   return (
-    <View>
-      <Text>PassengerSupportDrawer</Text>
+    <View style={{padding: 5}}>
+      <Text style={{fontWeight: 'bold', marginTop: 10, marginBottom: 10}}>
+        Support Contact Number
+      </Text>
+      <View
+        style={{
+          height: 44,
+          backgroundColor: '#cc2f2f',
+          margin: 5,
+          borderRadius: 22,
+        }}>
+        <TouchableOpacity onPress={makeCall}>
+          <Text
+            style={{
+              color: '#fff',
+              alignSelf: 'flex-start',
+              paddingTop: 8,
+              paddingLeft: 10,
+              fontWeight: 'bold',
+              fontSize: 20,
+            }}>
+            0767089918
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default PassengerSupportDrawer;
-
-const styles = StyleSheet.create({});
